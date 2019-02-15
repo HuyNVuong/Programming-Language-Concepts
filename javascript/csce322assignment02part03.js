92a508a15a0c5ade15855101061d9fc6d85ecf47
Huy Vuong <vuongnguyenhuy@yahoo.com>
2019-02-15 13:07:59 -0600

    part01, 02, 03, 04 added

---
+++ javascript/csce322assignment02part03.js
@@ -0,0 +1,14 @@
+module.exports = {
+    manyPlayersOneMove: manyPlayersOneMove
+}
+
+var helpers = require( './helpers' );
+
+function manyPlayersOneMove( game ){
+    
+    function whatever( move ){
+	return game;
+    }
+
+    return whatever;
+}
--- javascript/csce322assignment02part04.js
+++ javascript/csce322assignment02part04.js
@@ -1,5 +1,5 @@
 module.exports = {
-    onePlayerManyMoves: onePlayerManyMoves
+    manyPlayersManyMoves: manyPlayersManyMoves
 }
 
 var helpers = require( './helpers' );
@@ -17,7 +17,7 @@ class Point {
     }
 }
 
-function onePlayerManyMoves( game ){
+function manyPlayersManyMoves( game ){
     
     var playerSet = new Map();
     var preDef = new Map([[1, '1'], [2, '2'], [3, '3'], [4, '4'],
@@ -30,10 +30,12 @@ function onePlayerManyMoves( game ){
                 if(game[y][x] != '-') {
                     if (playerSet.has(game[y][x])) {
                         playerSet.get(game[y][x]).push(new Point(y, x));
+                        console.log(game[y][x], playerSet.get(game[y][x]));
                     } else {
                         var coorSet = new Array();
                         coorSet.push(new Point(y, x));
                         playerSet.set(game[y][x], coorSet);
+                        console.log(game[y][x], coorSet);
                     }
                 }
                 if (n < game[y][x]) {
@@ -46,42 +48,42 @@ function onePlayerManyMoves( game ){
 
     function checkWinner(playerPlayed) {
         var playerCoorSet = playerSet.get(playerPlayed);
-        console.log(playerCoorSet);
         var countX = new Map();
         var countY = new Map();
         var slopeCount = new Map();
         for (var p = 0; p < playerCoorSet.length; p++) {
             var point = playerCoorSet[p];
-            if (countX.has(point.x)) {
-                if (countX.get(point.x) >= 4) {
-                    return true;
-                } 
-                countX.set(point.x, countX.get(point.x) + 1);
-            } else {
-                countX.set(point.x, 1);
-            }
+            // if (countX.has(point.x)) {
+            //     countX.set(point.x, countX.get(point.x) + 1);
+            //     if (countX.get(point.x) >= 4) {
+            //         return true;
+            //     } 
+            // } else {
+            //     countX.set(point.x, 1);
+            // }
             if (countY.has(point.y)) {
+                countY.set(point.y, countY.get(point.y) + 1);
                 if (countY.get(point.y) >= 4) {
                     return true;
                 } 
-                countY.set(point.y, countY.get(point.y) + 1);
             } else {
                 countY.set(point.y, 1);
             }
-            for (var op = 0; op < playerCoorSet.length; op++) {
-                var other = playerCoorSet[op];
-                if(!point.equals(other)) {
-                    var slope = point.slopeTo(other);
-                    if (slopeCount.has(slope)) {
-                        if (slopeCount.get(slope) >= 4) {
-                            return true;
-                        }
-                        slopeCount.set(slope, slopeCount.get(slope) + 1);
-                    } else {
-                        slopeCount.set(slope, 1);
-                    }
-                }
-            }
+            // for (var op = 0; op < playerCoorSet.length; op++) {
+            //     var other = playerCoorSet[op];
+            //     if(!point.equals(other)) {
+            //         var slope = point.slopeTo(other);
+            //         if (slopeCount.has(slope)) {
+            //             if (slopeCount.get(slope) >= 4) {
+            //                 console.log('Player', playerPlayed, 'win by 4 crosses');
+            //                 return true;
+            //             }
+            //             slopeCount.set(slope, slopeCount.get(slope) + 1);
+            //         } else {
+            //             slopeCount.set(slope, 1);
+            //         }
+            //     }
+            // }
         }
         return false;
     }
@@ -92,17 +94,20 @@ function onePlayerManyMoves( game ){
         for (var p = 1; p <= n; p++) {
             players[p - 1] = p;
         }
-        var seen = new Set();
         for (var m = 0; m < moves.length; m++) {
             var move = moves[m];
             var player = preDef.get(players[m % parseInt(n, 10)]);
+            
             for(var i = 0; i < game.length; i++) {
+                // console.log(i, move - 1, game[i][move - 1]);
                 if (game[i][move - 1] != '-') {
                     game[i - 1][move - 1] = player;
                     break;
+                } else if (i == game.length - 1) {
+                    game[i][move - 1] = player;
+                    break;
                 }
             }
-            
             if (playerSet.has(player)) {
                 playerSet.get(player).push(new Point(i - 1, move - 1));
             } else {
@@ -113,10 +118,6 @@ function onePlayerManyMoves( game ){
             if(checkWinner(player)) {
                 break;
             }
-            seen.add(player);
-            if (seen.size == n) {
-                break;
-            }       
         }
         // console.log(playerSet);
         return game;      
