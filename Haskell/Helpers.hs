67d260a1f89ed2b30f7dcf309419742e61165383
Huy Vuong <vuongnguyenhuy@yahoo.com>
2019-03-15 16:39:45 -0500

    Couple functions added into helpers

--- Haskell/Helpers.hs
+++ Haskell/Helpers.hs
@@ -35,6 +35,18 @@ oneMoveHelper game player move
         row 			= findPlaceToMove colToPlay (length colToPlay)
         col				= move					
 
+manyPlayerHelper :: [[Char]] -> Char -> [Int] -> [[Char]]
+manyPlayerHelper game player moves 
+    | everyOnePlayed nextPlayer numPlayer == False   = manyPlayerHelper nextGame nextPlayer nextMove
+    | otherwise                                      = oneMoveHelper game nextPlayer nextMove 
+    where 
+        player     = '1'
+        nextPlayer = '2'
+        (nextMove, moves)   = getNextMove moves 
+        numPlayer   = findNumPlayer game 
+        nextGame    = oneMoveHelper game nextPlayer nextMove
+
+
 getRow :: [[Char]] -> Int -> [Char]
 getRow (row:rows) 0     = row 
 getRow (row:rows) n     = getRow rows (n - 1)
@@ -60,8 +72,8 @@ getElementInMatrix (row:rows) y x
     | y == 1	= getElement row x 
     | otherwise = getElementInMatrix rows (y - 1) x
 
-getNextMove :: [Int] -> Int 
-getNextMove (h:t)	= h 
+getNextMove :: [Int] -> (Int, [Int]) 
+getNextMove (h:t)	= (h, t)
 
 getElement :: [Char] -> Int -> Char 
 getElement (el:els) 1	= el
@@ -70,3 +82,49 @@ getElement (el:els) n 	= getElement (els) (n - 1)
 getCol :: [[Char]] -> Int -> [Char] 
 getCol [row] col		= [(getElement row col)]
 getCol (row:rows) col 	= (getElement row col):(getCol rows col)
+
+findNextPlayer :: Char -> Int -> Char
+findNextPlayer player numPlayer = 
+    intToChar (((charToInt player) + 1) `mod` (numPlayer + 1))
+
+findNumPlayer :: [[Char]] -> Int
+findNumPlayer [row]     = maxFromList row 
+findNumPlayer (row:rows)
+    | findNumPlayer rows > maxFromList row  = findNumPlayer rows 
+    | otherwise                             = maxFromList row
+
+everyOnePlayed :: Char -> Int -> Bool 
+everyOnePlayed player numPlayer 
+    | (charToInt player) == numPlayer   = True 
+    | otherwise                         = False
+
+maxFromList :: [Char] -> Int 
+maxFromList [x]         = charToInt x 
+maxFromList (x:xs)
+    | (maxFromList xs) > (charToInt x)  = maxFromList xs 
+    | otherwise                         = charToInt x
+
+intToChar :: Int -> Char 
+intToChar num 
+    | num == 1  = '1'
+    | num == 2  = '2'
+    | num == 3  = '3'
+    | num == 4  = '4'
+    | num == 5  = '5'
+    | num == 6  = '6'
+    | num == 7  = '7'
+    | num == 8  = '8'
+    | num == 9  = '9'
+
+charToInt :: Char -> Int 
+charToInt player 
+    | player == '1'  = 1
+    | player == '2'  = 2
+    | player == '3'  = 3
+    | player == '4'  = 4
+    | player == '5'  = 5
+    | player == '6'  = 6
+    | player == '7'  = 7
+    | player == '8'  = 8
+    | player == '9'  = 9
+    | otherwise      = 0
