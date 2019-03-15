a7191fa6592b171bab8ddf23392b686bcc68cd17
Huy Vuong <vuongnguyenhuy@yahoo.com>
2019-03-15 14:33:25 -0500

    part 01 finished

--- Haskell/Helpers.hs
+++ Haskell/Helpers.hs
@@ -1,6 +1,8 @@
 module Helpers
 ( readConnectFourFile
 , printGame
+, onePlayerHelper
+, putPieceToGame
 ) where
 
 import Prelude
@@ -14,6 +16,61 @@ readConnectFourFile = readIO
 printGame :: [[Char]] -> IO () 
 printGame [] = do 
 	print ""
+
 printGame (row:rows) = do	
 	print row
 	printGame rows
+
+
+----------------------------------------------------------------------------
+-- Code, Logic implementation to play the connect Four in variations -------
+----------------------------------------------------------------------------
+
+onePlayerHelper :: [[Char]] -> Char -> Int -> [[Char]]
+onePlayerHelper game player move 
+    | isEmptyPlace colToPlay move == True	= putPieceToGame game player row col
+    | otherwise								= putPieceToGame game player row col
+    where 
+        colToPlay		= getCol game move
+        row 			= findPlaceToMove colToPlay (length colToPlay)
+        col				= move					
+
+getRow :: [[Char]] -> Int -> [Char]
+getRow (row:rows) 0     = row 
+getRow (row:rows) n     = getRow rows (n - 1)
+
+findPlaceToMove :: [Char] -> Int -> Int 
+findPlaceToMove row n
+	| isEmptyPlace row n == True	= n - 1
+	| otherwise						= findPlaceToMove row (n - 1)
+
+putPieceToGame :: [[Char]] -> Char -> Int -> Int -> [[Char]]
+putPieceToGame [] _ _ _ = []
+putPieceToGame game player y x = take y game 
+    ++ [take x (game !! y) ++ [player] ++ drop (x + 1) (game !! y)] 
+    ++ drop (y + 1) game 
+
+isEmptyPlace :: [Char] -> Int -> Bool 
+isEmptyPlace colToPlay row  
+    | getElement colToPlay row == '-'	= True 
+    | otherwise							= True 
+
+getElementInMatrix :: [[Char]] -> Int -> Int -> Char 
+getElementInMatrix (row:rows) y x 
+    | y == 1	= getElement row x 
+    | otherwise = getElementInMatrix rows (y - 1) x
+
+reverseList :: [[Char]] -> [[Char]] 
+reverseList []      = []
+reverseList (x:xs)  = (reverseList xs) ++ [x]
+
+getNextMove :: [Int] -> Int 
+getNextMove (h:t)	= h 
+
+getElement :: [Char] -> Int -> Char 
+getElement (el:els) 1	= el
+getElement (el:els) n 	= getElement (els) (n - 1)
+
+getCol :: [[Char]] -> Int -> [Char] 
+getCol [row] col		= [(getElement row col)]
+getCol (row:rows) col 	= (getElement row col):(getCol rows col)
--- Haskell/csce322assignment03part01.hs
+++ Haskell/csce322assignment03part01.hs
@@ -10,61 +10,13 @@ main = do
     (game,moves) <- readConnectFourFile filename
     print "Result"
     let result = onePlayerOneMove game (head moves)
+    --- Debugging
+    -- let result = putPieceToGame game '5' 8 (head moves)
+    --- End of debugging   
     printGame result
 
 -- YOUR CODE SHOULD COME AFTER THIS POINT
 
 
 onePlayerOneMove :: [[Char]] -> Int -> [[Char]]
-onePlayerOneMove game move = game 
-        where game = reverseList RevGame
-                where 
-                revGame = onePlayerHelper revGame '1' move
-                revGame = reverseList game 
-                revGame = putCol move 
-
-reverseList :: [[Char]] -> [[Char]] 
-reverseList []      = []
-reverseList (x:xs)  = (reverseList xs) ++ [x]
-                
-onePlayerHelper :: [[Char]] -> Char -> Int -> [[Char]]
-onePlayerHelper [] _ _  = []
-
-getElement :: Int -> [Char] -> Char 
-getElement 1 (el:els)   = el 
-getElement n (el:els)    = getElement (n - 1) els 
-
-dropElement :: Int -> [Char] -> [Char]
-dropElement _ []        = []
-dropElement i row       = left ++ right 
-        where (left, (_:right)) = Data.List.splitAt i row
-
-putElement :: Int -> Char -> [Char] -> [Char] 
-putElement _ _ []       = []
-putElement n p (y:ys)
-        | n == 0        = (p:ys)
-        | otherwise     = y:putElement (n - 1) p ys 
-
-getCol :: Int -> [[Char]] -> [Char]
-getCol col [row]        = [(getElement col row)]    
-getCol col (row:rows)   = (getElement col row):(getCol col rows)
-
-putCol :: Int -> [Char] -> [[Char]] -> [[Char]]
-putCol _ [] []          = [] 
-
-dropCol :: Int -> [[Char]] -> [[Char]] 
-dropCol _ []            = [] 
-
-getRow :: [[Char]] -> Int -> [Char]
-getRow (row:rows) 0     = row 
-getRow (row:rows) n     = getRow rows (n - 1)
-
--- https://stackoverflow.com/questions/5852722/replace-individual-list-elements-in-haskell
-
-putPiece :: Int -> (Char -> Char) -> [[Char]] -> [[Char]]
-putPiece _ _ []         = []
-
-
-
-
-    
+onePlayerOneMove game move = onePlayerHelper game '1' (move - 1)
