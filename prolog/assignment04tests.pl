528be9980797f6c46ea040eba258b63403b3f859
Huy Vuong <vuongnguyenhuy@yahoo.com>
2019-03-25 17:41:15 -0500

    prolog files added

---
+++ prolog/assignment04tests.pl
@@ -0,0 +1,35 @@
+printSequences([]).
+printSequences([Sequence|Sequences]):-
+    writeln(Sequence),
+    printSequences(Sequences).
+
+loadHelpers:-
+    ['helpers'],
+    ['csce322assignment04part01'],
+    ['csce322assignment04part02'],
+    ['csce322assignment04part03'],
+    ['csce322assignment04part04'].
+    
+part01:-
+    readConnectFourFile('part01test01.cnf',Moves,Game),
+    writeln(moves),
+    writeln(Moves),
+    printGame(Game),
+    columnsAndMoves(Game,Moves).
+    
+part02:-
+    readConnectFourFile('part01test01.cnf',_,Game),
+    printGame(Game),
+    occupiedSpaces(Game).
+    
+part03:-
+    readConnectFourFile('part01test01.cnf',_,Game),
+    printGame(Game),
+    setof(Moves,fewestMoves(Game,Moves),AllMoves),
+    writeln(moves),
+    printSequences(AllMoves).
+    
+part04:-
+    readConnectFourFile('part01test01.cnf',_,Game),
+    printGame(Game),
+    notDiagonal(Game).
