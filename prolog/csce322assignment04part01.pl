dcaf874a227eaa8445b10bebbba9c916b9b8e7e3
Huy Vuong <vuongnguyenhuy@yahoo.com>
2019-03-25 19:03:57 -0500

    helpers added

--- prolog/csce322assignment04part01.pl
+++ prolog/csce322assignment04part01.pl
@@ -1,1 +1,2 @@
 columnsAndMoves(Game,Moves).
+between(1,10,X) 0 is mod (X, 2).
---
+++ prolog/helpers.pl
@@ -0,0 +1,30 @@
+:- module( helpers,
+	 [ readConnectFourFile/3
+	 , printGame/1
+	 ]
+    ).
+
+readConnectFourFile(File,Moves,Game):-
+    open(File,read,Input),
+    read(Input,Moves),
+    readGame(Input,Game),
+    close(Input).
+
+readGame(Input,[]):-
+    at_end_of_stream(Input),
+    !.
+    
+readGame(Input,[Row|Rows]):-
+    \+ at_end_of_stream(Input),
+    read(Input,Row),
+    readGame(Input,Rows).
+
+printGame(Game):-
+    writeln(game),
+    printRows(Game).
+    
+printRows([]).
+printRows([Row|Rows]):-
+    writeln(Row),
+    printRows(Rows).
+
