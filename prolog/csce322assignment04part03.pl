2576d74dfa57b7b258add31ae709a8066bef680c
Huy Vuong <vuongnguyenhuy@yahoo.com>
2019-04-23 14:28:02 -0500

    Logic for winning a Row updated. Sorted all the winning combination and determine a shortest one implemented

--- prolog/csce322assignment04part03.pl
+++ prolog/csce322assignment04part03.pl
@@ -1,41 +1,66 @@
 fewestMoves(Game, Moves):-
-    findPlayer(Game, 1, Row, Col),
-    RowFrom is Row - 1,
-    RowTo is Row - 3,
-    (pathToWinCol(Col, RowFrom, RowTo, Game, Moves);
-    pathToWinRow(Game, Row, Col, Moves))
+    findAllMoves(Game, Moves),
+    findAllMovesWithLength(Game, MovesWithLength),
+    getShortestLength(MovesWithLength, (ShortestLength, _)),
+    length(Moves, MovesLength),
+    MovesLength = ShortestLength
     .
 
-pathToWinCol(Col, RowTo, RowTo, _, [Col]).
+getShortestLength([(Length,Move)|_], (Length, Move)).
 
-pathToWinCol(Col, RowFrom, RowTo, Game, NewMoves):-
+findAllMovesWithLength(Game, MovesWithLength):-
+    setof((Length, Moves),
+          (findAllMoves(Game, Moves), length(Moves, Length)),
+          (MovesWithLength)).
+
+findAllMoves(Game, Moves):-
+    findPlayer(Game, 1, Row, Col),
+    (
+        pathToWinCol(Game, Row, Col, Moves);
+        pathToWinRow(Game, Row, Col, Moves)
+    )
+    .
+
+pathToWinCol(Game, Row, Col, Moves):-
+    RowFrom is Row - 1,
+    RowTo is Row - 3,
+    pathToWinColHelper(Col, RowFrom, RowTo, Game, Moves).
+
+pathToWinColHelper(Col, RowTo, RowTo, _, [Col]).
+
+pathToWinColHelper(Col, RowFrom, RowTo, Game, NewMoves):-
     RowFrom >= RowTo,
     RowFromM1 is RowFrom - 1,
     (
         (at(Game, RowFrom, Col, -),
-        pathToWinCol(Col, RowFromM1, RowTo, Game, Moves),
+        pathToWinColHelper(Col, RowFromM1, RowTo, Game, Moves),
         append([Col], Moves, NewMoves));
         (at(Game, RowFrom, Col, 1),
-        pathToWinCol(Col, RowFromM1, RowTo, Game, Moves),
+        pathToWinColHelper(Col, RowFromM1, RowTo, Game, Moves),
         append([], Moves, NewMoves))
     )
     .
 
-pathToWinRow(Game, Row, Col, Moves):-
+pathToWinRow(Game, Row, Col, AllMoves):-
     (
-        (ColFrom is Col - 3, ColTo is Col - 1,
-        pathToWinRowHelper(Row, ColFrom, ColTo, Game, Moves));
+        (ColFrom is Col - 3, ColTo is Col,
+        pathToWinRowHelper(Row, ColFrom, ColTo, Game, Moves),
+        permutation(Moves, AllMoves));
         (ColFrom is Col - 2, ColTo is Col + 1,
-        pathToWinRowHelper(Row, ColFrom, ColTo, Game, Moves));
+        pathToWinRowHelper(Row, ColFrom, ColTo, Game, Moves),
+        permutation(Moves, AllMoves));
         (ColFrom is Col - 1, ColTo is Col + 2,
-        pathToWinRowHelper(Row, ColFrom, ColTo, Game, Moves));
+        pathToWinRowHelper(Row, ColFrom, ColTo, Game, Moves),
+        permutation(Moves, AllMoves));
         (ColFrom is Col, ColTo is Col + 3,
-        pathToWinRowHelper(Row, ColFrom, ColTo, Game, Moves))
+        pathToWinRowHelper(Row, ColFrom, ColTo, Game, Moves),
+        permutation(Moves, AllMoves))
     )
     .
 
+pathToWinRowHelper(Row, ColTo, ColTo, Game, [ColTo]):- at(Game, Row, ColTo, -).
 
-pathToWinRowHelper(Row, ColTo, ColTo, _, [Row]).
+pathToWinRowHelper(Row, ColTo, ColTo, Game, []):- at(Game, Row, ColTo, 1).
 
 pathToWinRowHelper(Row, ColFrom, ColTo, Game, NewMoves):-
     ColTo >= ColFrom,
