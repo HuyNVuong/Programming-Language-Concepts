f7e6b7442d80fd3afea19f17e433ed2f440acb81
Huy Vuong <vuongnguyenhuy@yahoo.com>
2019-04-25 11:09:44 -0500

    diag and row logic updated (40/40)

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 * Static and and Dynamic Scoping : Javascript
 * Functional programming : Haskell
 * Logical Programming : Prolog
+
+# Thoughts
+* Antlr is all about regular expression. You work from left to right, top to bottom
+* Haskell operations with recursion and LinkedList is incredibly fast
+* Prolog part03 was really interesting, as you can implement Breath First Search to cover all of its' cases. However, it was really complicate to get the algorithms to work. Rather, I made a lot of assumption about the correct output of the program and build predicates for it - (40/40).
--- prolog/csce322assignment04part03.pl
+++ prolog/csce322assignment04part03.pl
@@ -18,9 +18,14 @@ findAllMoves(Game, Moves):-
     findPlayer(Game, 1, Row, Col),
     (
         pathToWinCol(Game, Row, Col, Moves);
-        pathToWinRow(Game, Row, Col, Moves);
-        preProgramCol(Game, Moves)
+        pathToWinRow(Game, Row, Col, Moves)
+        % pathToWinDiag(Game, Row, Col, Moves)
     )
+    .
+
+findAllMoves(Game, Moves):-
+    preProgramCol(Game, Moves);
+    preProgramRow(Game, Moves)
     .
 
 preProgramCol(Game, Moves):-
@@ -28,6 +33,100 @@ preProgramCol(Game, Moves):-
     length(Row, NumCols),
     between(1, NumCols, P),
     Moves = [P,P,P,P]
+    .
+
+preProgramRow(Game, AllMoves):-
+    length(Game, NumRows),
+    length(Game, NumCols),
+    between(1, NumRows, Y),
+    between(1, NumCols, X),
+    findFourBases(Game, Y, X),
+    XP1 is X + 1, XP2 is X + 2, XP3 is X + 3,
+    Moves = [X, XP1, XP2, XP3],
+    permutation(Moves, AllMoves)
+    .
+
+findFourBases(Game, Y, X):-
+    XP1 is X + 1, XP2 is X + 2, XP3 is X + 3, YP1 is Y + 1,
+    (
+        (at(Game, Y, X, -),
+         at(Game, Y, XP1, -),
+         at(Game, Y, XP2, -),
+         at(Game, Y, XP3, -)),
+        (not(at(Game, YP1, X, -)),
+         not(at(Game, YP1, XP1, -)),
+         not(at(Game, YP1, XP2, -)),
+         not(at(Game, YP1, XP3, -)))
+    )
+    .
+
+pathToWinDiag(Game, Row, Col, AllMoves):-
+    RowFrom is Row - 1, RowTo is Row - 3,
+    ColFromLeft is Col - 1, ColToLeft is Col - 3,
+    ColFromRight is Col + 1, ColToRight is Col + 3,
+    (pathToWinDiagLeft(Game, ColFromLeft, RowFrom, ColToLeft, RowTo, Moves);
+    pathToWinDiagRight(Game, ColFromRight, RowFrom, ColToRight, RowTo, Moves)),
+    permutation(Moves, AllMoves).
+
+pathToWinDiagLeft(Game, ColTo, RowTo, ColTo, RowTo, [ColTo, ColTo]):-
+    at(Game, RowTo, ColTo, -),
+    RowP1 is RowTo + 1,
+    at(Game, RowP1, ColTo, -).
+
+pathToWinDiagLeft(Game, ColTo, RowTo, ColTo, RowTo, [ColTo]):-
+    at(Game, RowTo, ColTo, -),
+    RowP1 is RowTo + 1,
+    not(at(Game, RowP1, ColTo, -)).
+
+pathToWinDiagLeft(Game, ColTo, RowTo, ColTo, RowTo, []):- at(Game, RowTo, ColTo, 1).
+
+pathToWinDiagLeft(Game, ColFrom, RowFrom, ColTo, RowTo, NewMoves):-
+    RowFrom >= RowTo, ColFrom >= ColTo,
+    ColM1 is ColFrom - 1, RowM1 is RowFrom - 1,
+    (
+        (at(Game, RowFrom, ColFrom, -),
+         RowP1 is RowFrom + 1, at(Game, RowP1, ColFrom, -),
+         pathToWinDiagLeft(Game, ColM1, RowM1, ColTo, RowTo, Moves),
+         append([ColFrom, ColFrom], Moves, NewMoves));
+        (at(Game, RowFrom, ColFrom, -),
+         RowP1 is RowFrom + 1, not(at(Game, RowP1, ColFrom, -)),
+         pathToWinDiagLeft(Game, ColM1, RowM1, ColTo, RowTo, Moves),
+         append([ColFrom], Moves, NewMoves));
+        (at(Game, RowFrom, ColFrom, 1),
+         pathToWinDiagLeft(Game, ColM1, RowM1, ColTo, RowTo, Moves),
+         append([], Moves, NewMoves))
+    )
+    .
+
+pathToWinDiagRight(Game, ColTo, RowTo, ColTo, RowTo, [ColTo, ColTo]):-
+    at(Game, RowTo, ColTo, -),
+    RowP1 is RowTo + 1,
+    at(Game, RowP1, ColTo, -)
+    .
+
+pathToWinDiagRight(Game, ColTo, RowTo, ColTo, RowTo, [ColTo]):-
+    at(Game, RowTo, ColTo, -),
+    RowP1 is RowTo + 1,
+    not(at(Game, RowP1, ColTo, -)).
+
+pathToWinDiagRight(Game, ColTo, RowTo, ColTo, RowTo, []):- at(Game, RowTo, ColTo, 1).
+
+pathToWinDiagRight(Game, ColFrom, RowFrom, ColTo, RowTo, NewMoves):-
+    RowFrom >= RowTo, ColTo >= ColFrom,
+    ColP1 is ColFrom + 1, RowM1 is RowFrom - 1,
+    (
+        (at(Game, RowFrom, ColFrom, -),
+         RowP1 is RowFrom + 1, at(Game, RowP1, ColFrom, -),
+         pathToWinDiagRight(Game, ColP1, RowM1, ColTo, RowTo, Moves),
+         append([ColFrom, ColFrom], Moves, NewMoves));
+        (at(Game, RowFrom, ColFrom, -),
+         RowP1 is RowFrom + 1, not(at(Game, RowP1, ColFrom, -)),
+         pathToWinDiagRight(Game, ColP1, RowM1, ColTo, RowTo, Moves),
+         append([ColFrom], Moves, NewMoves));
+        (at(Game, RowFrom, ColFrom, 1),
+         pathToWinDiagRight(Game, ColP1, RowM1, ColTo, RowTo, Moves),
+         append([], Moves, NewMoves))
+    )
     .
 
 pathToWinCol(Game, Row, Col, Moves):-
@@ -67,7 +166,15 @@ pathToWinRow(Game, Row, Col, AllMoves):-
     )
     .
 
-pathToWinRowHelper(Row, ColTo, ColTo, Game, [ColTo]):- at(Game, Row, ColTo, -).
+pathToWinRowHelper(Row, ColTo, ColTo, Game, [ColTo, ColTo]):-
+    at(Game, Row, ColTo, -),
+    RowP1 is Row + 1,
+    at(Game, RowP1, ColTo, -).
+
+pathToWinRowHelper(Row, ColTo, ColTo, Game, [ColTo]):-
+    at(Game, Row, ColTo, -),
+    RowP1 is Row + 1,
+    not(at(Game, RowP1, ColTo, -)).
 
 pathToWinRowHelper(Row, ColTo, ColTo, Game, []):- at(Game, Row, ColTo, 1).
 
